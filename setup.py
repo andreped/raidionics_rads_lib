af2570a24156266e4f59232b1b11fb11fc5c0ea8
André Pedersen <andrped94@gmail.com>
2023-10-03 18:03:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 with open("README.md", "r", errors='ignore') as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r', encoding='utf-16', errors='ignore') as ff:
+with open('requirements.txt', 'r', encoding='utf-16-le', errors='ignore') as ff:
     required = ff.read().splitlines()
 
 if platform.system() == 'Windows':
